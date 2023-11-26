249d092e7709ce894790e028bbcaa3056a7fcfff
Wojciech <kowalczyk.wojtek168@gmail.com>
2023-11-27 00:48:40 +0100

    sth is wrong with form body (discord api error 50035)

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,43 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+
+      const { commands, commandArray } = client;
+
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
+        console.log(
+          `Command: ${command.data.name} has been passed through the handler`
+        );
+      }
+    }
+
+    const clientId = "1172256693351108689";
+    const guildId = "1172256500933201991";
+
+    const rest = new REST({ version: "9" }).setToken(process.env.token);
+
+    try {
+      console.log("Started refreshing application (/) commands.");
+
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: client.commandArray,
+      });
+
+      console.log("Succesfully reloaded application (/) commands.");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
